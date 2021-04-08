1ce40b7fa56fd1ef672bb91f6eb440863572189d
kotlaki <voland171@mail.ru>
2021-04-08 22:27:24 +0500

    Project finish

---
+++ src/main/java/org/kurganov/camelbase64/CamelBase64Application.java
@@ -0,0 +1,13 @@
+package org.kurganov.camelbase64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelBase64Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelBase64Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/kurganov/camelbase64/CamelBase64ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kurganov.camelbase64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelBase64ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
